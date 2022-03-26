2077a6c2abf8eda184be7b9e742dc642f60004bc
Dennnhhhickk <iseleh09@gmail.com>
2022-03-26 20:01:55 +0300

    Initial Commit

---
+++ arithmetic/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  arithmetic
+//
+//  Created by Денис Ядерцов on 26.03.2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
